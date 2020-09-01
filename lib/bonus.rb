38479734a8fb037995ab5c2659969347628a8989
Drew Thompson <drewallynthompson@gmail.com>
2020-09-01 00:26:59 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,8 @@ def bonus
   }
 
   
-epic_tradgedy[:montague][:hero][:status] = "dead"
-epic_tradgedy[:capulet][:heroine][:status] = "dead"
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
